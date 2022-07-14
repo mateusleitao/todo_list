20fd0875dae721bb917de7671155e8f768a0c13d
mateusleitao <mateuselpidio0@gmail.com>
2022-07-13 23:31:43 -0300

    todo list finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+
+import { ITask } from "./interfaces/Task";
+import Modal from "./components/Modal";
 
 function App() {
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+  const [taskToUpdate, setTaskToUpdate] = useState<ITask | null>(null);
+
+  const deleteTask = (id: number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id;
+      })
+    );
+  };
+
+  const hideOrShowModal = (display: boolean) => {
+    const modal = document.querySelector("#modal");
+    if (display) {
+      modal!.classList.remove("hide");
+    } else {
+      modal!.classList.add("hide");
+    }
+  };
+
+  const editTask = (task: ITask): void => {
+    hideOrShowModal(true);
+    setTaskToUpdate(task);
+  };
+
+  const updateTask = (id: number, title: string, difficulty: number) => {
+    const updatedTask: ITask = { id, title, difficulty };
+
+    const updatedItems = taskList.map((task) => {
+      return task.id === updatedTask.id ? updatedTask : task;
+    });
+
+    setTaskList(updatedItems);
+
+    hideOrShowModal(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Modal
+        children={
+          <TaskForm
+            btnText="Edit Task"
+            taskList={taskList}
+            task={taskToUpdate}
+            handleUpdate={updateTask}
+          />
+        }
+      ></Modal>
+      <Header></Header>
+      <main className={styles.main}>
+        <div>
+          <h2>What will you do?</h2>
+          <TaskForm
+            btnText="Create Task"
+            taskList={taskList}
+            setTaskList={setTaskList}
+          />
+        </div>
+        <div>
+          <h2>Your tasks:</h2>
+          <TaskList
+            taskList={taskList}
+            handleDelete={deleteTask}
+            handleEdit={editTask}
+          />
+        </div>
+      </main>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styles from "./Footer.module.css"
+
+
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className={styles.footer}>
+        <p>
+          <span>Lange</span> @ 2022
+        </p>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
